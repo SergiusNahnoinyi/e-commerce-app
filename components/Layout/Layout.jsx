ded227bbe318353df1b89b6ef3f56f781f996ef5
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-09 14:11:44 +0200

    Release: create a footer component

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 
+import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 export default function Layout({ children }) {
@@ -15,7 +16,7 @@ export default function Layout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>Header</header>
+      <Header />
       <main>{children}</main>
       <Footer />
     </>
