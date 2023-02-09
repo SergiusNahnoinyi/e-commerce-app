b36a2cd80235c66beb410ce2e59aae4751fb4e37
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-09 11:24:54 +0200

    Feature: create an initial product item

---
+++ components/Product/Product.jsx
@@ -0,0 +1,5 @@
+import styles from "./Product.module.css";
+
+export default function Product() {
+  return <li className={styles.product}>Product</li>;
+}
--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -1,5 +1,11 @@
+import Product from "../Product/Product";
+
 import styles from "./Products.module.css";
 
 export default function Products() {
-  return <ul className={styles.products}>Products</ul>;
+  return (
+    <ul className={styles.products}>
+      <Product />
+    </ul>
+  );
 }
