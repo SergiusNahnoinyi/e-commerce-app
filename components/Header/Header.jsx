95118c3bb0c718cd6b095fba84e15e7e39a12ef1
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-09 14:31:28 +0200

    Style: add styles to the header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+
 import styles from "./Header.module.css";
 
 export default function Header() {
-  return <header className={styles.header}>Header</header>;
+  return (
+    <header className={styles.header}>
+      <Link href="/">E-commerce</Link>
+      <button
+        type="button"
+        className={styles.cart__button}
+        aria-label="Go to the list of purchases"
+        onClick={() => console.log("Click")}
+      >
+        <AiOutlineShopping aria-hidden="true" />
+        <span className={styles.cart__quantity}>1</span>
+      </button>
+    </header>
+  );
 }
