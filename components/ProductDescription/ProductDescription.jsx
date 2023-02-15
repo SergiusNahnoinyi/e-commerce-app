08fc605abf054be180f18511d3f149c70cc4ac4a
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-15 10:35:29 +0200

    Feature: add an image

--- components/ProductDescription/ProductDescription.jsx
+++ components/ProductDescription/ProductDescription.jsx
@@ -1,13 +1,29 @@
+import Img from "next/image";
+
+import { useNextSanityImage } from "next-sanity-image";
+
+import { client } from "@/services/sanity";
+
 import Button from "../Button/Button";
 
 import styles from "./ProductDescription.module.css";
 
 export default function ProductDescription({ product }) {
+  const imageProps = useNextSanityImage(client, product.image[0]);
+
   if (!product)
     return <h2 style={{ textAlign: "center" }}>No data provided</h2>;
 
   return (
     <section className={styles.description}>
+      <Img
+        {...imageProps}
+        style={{ maxWidth: "100%", height: "auto" }}
+        placeholder="blur"
+        blurDataURL={product.image[0].asset._ref}
+        className={styles.description__image}
+        alt={product.name}
+      />
       <h1 className={styles.product}>{product.name}</h1>
       <ul className={styles.buttons}>
         <li>
