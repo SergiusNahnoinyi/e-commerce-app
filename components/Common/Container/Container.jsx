5e3945cadc2b3a742796d6f4ff238cdeed1fbdb1
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-22 09:13:54 +0200

    Styles: add styles to the container

--- components/Common/Container/Container.jsx
+++ components/Common/Container/Container.jsx
@@ -2,6 +2,6 @@ import clsx from "clsx";
 
 import styles from "./Container.module.css";
 
-export default function Container({ children }) {
+export default function Container({ children, className }) {
   return <div className={clsx(styles.container, className)}>{children}</div>;
 }
