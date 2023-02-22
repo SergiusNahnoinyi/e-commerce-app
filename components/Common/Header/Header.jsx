4141ec11b5745f884c6168377109e1f398b8b5c5
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-22 09:42:24 +0200

    Refactoring: move a cart button to the separate file

--- components/Common/Header/Header.jsx
+++ components/Common/Header/Header.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { AiOutlineShopping } from "react-icons/ai";
 
 import Container from "../Container";
+import CartButton from "../CartButton";
 
 import styles from "./Header.module.css";
 
@@ -10,15 +10,7 @@ export default function Header() {
     <header>
       <Container className={styles.header__container}>
         <Link href="/">E-commerce</Link>
-        <button
-          type="button"
-          className={styles.cart__button}
-          aria-label="Go to the list of purchases"
-          onClick={() => console.log("Click")}
-        >
-          <AiOutlineShopping aria-hidden="true" />
-          <span className={styles.cart__quantity}>1</span>
-        </button>
+        <CartButton onClick={() => console.log("Click")} />
       </Container>
     </header>
   );
